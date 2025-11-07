bab9b1a0764557fe126a777f6f887e747f8c2388
Luke Davis <dowhileluke@gmail.com>
2025-11-06 18:51:51 -0800

    4x

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,20 +1,14 @@
+import { Categories } from './categories'
 import { Controls } from './controls'
-import { Major } from './major'
-import { Minor } from './minor'
-import { Normal } from './normal'
 
 export function App() {
     return (
         <>
-            <main className="flex-center flex-wrap gap-2">
-                <section className="flex-center flex-col gap-2">
-                    <Major />
-                    <Minor />
-                </section>
-
-                <section className="flex-center">
-                    <Normal />
-                </section>
+            <main className="flex gap-12">
+                <Categories />
+                <Categories />
+                <Categories />
+                <Categories />
             </main>
             <Controls />
         </>
---
+++ src/components/categories.tsx
@@ -0,0 +1,18 @@
+import { Major } from './major'
+import { Minor } from './minor'
+import { Normal } from './normal'
+
+export function Categories() {
+    return (
+        <main className="flex-center flex-col gap-2">
+            <section className="flex-center flex-col gap-2">
+                <Major />
+                <Minor />
+            </section>
+
+            <section className="flex-center">
+                <Normal />
+            </section>
+        </main>
+    )
+}
