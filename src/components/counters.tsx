291632dc735f6c2fd7cbe4ef9aa42e4140c7cddd
Luke Davis <dowhileluke@gmail.com>
2025-11-28 14:57:38 -0800

    goal

--- src/components/counters.tsx
+++ src/components/counters.tsx
@@ -1,3 +1,4 @@
+import { StarIcon } from '@phosphor-icons/react'
 import { EyesTable } from './eyes-table'
 import { Section } from './section'
 
@@ -5,6 +6,10 @@ export function Counters() {
     return (
         <Section name="Values" isPlain>
             <EyesTable />
+            <div className="flex-center text-xs text-stone-400">
+                <span>Goal: Capture <strong>61</strong></span>
+                <StarIcon size="1em" weight="fill" />
+            </div>
         </Section>
     )
 }
