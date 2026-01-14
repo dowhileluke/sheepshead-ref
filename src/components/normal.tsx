6a997bde9e7e3eabfe920f64460c03532d426aef
Luke Davis <dowhileluke@gmail.com>
2026-01-13 21:46:27 -0800

    dots

--- src/components/normal.tsx
+++ src/components/normal.tsx
@@ -4,21 +4,25 @@ import { useAppState } from '../hooks/use-app-state'
 import { List } from './list'
 import { MultiCard } from './multi-card'
 import { Section } from './section'
+import { DotIcon } from '@phosphor-icons/react'
 
 export function Normal() {
     const [state] = useAppState()
     const { icons, colors, } = LIBRARY[state.deck]
+    const plainSuitIndexes = FOUR.filter(n => n !== state.trump)
 
     const note = (
-        <div className="flex-center gap-1 p-1 rounded-full border border-stone-200 backdrop-blur-md">
-            {FOUR.map(i => i === state.trump ? null : createElement(
-                icons[i],
-                {
-                    className: colors[i],
-                    weight: 'fill',
-                    size: '0.8em',
-                },
-            ))}
+        <div className="flex-center gap-0 py-0.5 px-1 rounded-full border border-stone-300 bg-stone-100">
+            {plainSuitIndexes.map(i => {
+                const SuitIcon = icons[i]
+
+                return (
+                    <>
+                        <SuitIcon weight="fill" size="0.8em" className={colors[i]} />
+                        <DotIcon weight="bold" size="0.8em" className="text-stone-500 last:hidden" />
+                    </>
+                )
+            })}
         </div>
     )
 
